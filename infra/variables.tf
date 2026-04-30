309be3c879c715feab71847e9f91b0121f835ad7
Felipe Jara <fjararibet@gmail.com>
2026-04-30 01:25:00 -0400

    initial commit

---
+++ infra/variables.tf
@@ -0,0 +1,21 @@
+variable project_id {
+  type = string
+}
+variable deployment_id {
+  type = string
+}
+variable im_deployment_repo_uri {
+  type = string
+}
+variable im_deployment_ref {
+  type = string
+}
+
+variable github_app_installation_id {
+  type = string
+}
+
+variable github_personal_access_token {
+  type = string
+  sensitive = true
+}
