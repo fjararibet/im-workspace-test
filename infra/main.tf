44936f4382f1e65b831244d5980b1b722f26e4b4
Felipe Jara <fjararibet@gmail.com>
2026-04-30 02:01:06 -0400

    default job

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = "us-central1"
+}
+
 module "im-workspace" {
   source = "./im-workspace/modules/im_cloudbuild_workspace"
 
@@ -11,4 +25,18 @@ module "im-workspace" {
 
   github_app_installation_id = var.github_app_installation_id
   github_personal_access_token = var.github_personal_access_token
+}
+
+resource "google_cloud_run_v2_job" "default" {
+  name     = "cloudrun-job"
+  location = "us-central1"
+  deletion_protection = false
+
+  template {
+    template {
+      containers {
+        image = "us-docker.pkg.dev/cloudrun/container/job"
+      }
+    }
+  }
 }
